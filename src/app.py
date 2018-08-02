b0be758d137ec9d885100cfc76bb1839d86d46d6
Adnan Issadeen <eddyrox1@hotmail.com>
2018-08-02 16:15:06 +0530

    Added a slow running request

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import time
+import logging
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Worlds!'
+
+@app.route('/slow')
+def hello_slow_world():
+    logger = logging.getLogger('app')
+    logger.warning("starting")
+    time.sleep(6)
+    logger.warning("done sleeping")
+    return 'Hello, slow Worlds!'
